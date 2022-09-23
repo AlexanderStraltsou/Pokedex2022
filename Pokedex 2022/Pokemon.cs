4678d8fe247c5003eeaab589ba867f7204cb8170
AlexanderStraltsou <113332203+AlexanderStraltsou@users.noreply.github.com>
2022-09-23 08:55:28 +0200

    Initial commit

---
+++ Pokedex 2022/Pokemon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokedex_2022
+{
+    internal class Pokemon
+    {
+        public string name;
+        public string type;
+        public int currentForm;
+        public int totalForms;
+
+        public Pokemon()
+        {
+
+        }
+
+    }
+}
---
+++ Pokedex 2022/Program.cs
@@ -0,0 +1,30 @@
+﻿//Evolva
+//Fråga användaren om vilken pokemon den vill evolva
+//Hitta rätt påkemon i listan
+//Evolva den pokemon genom en metod i dess klass
+//Displaya listan med pokemons
+
+
+using Class1;
+
+List <Pokemon> pokemons = new ();
+
+PopulatePokemonsList();
+
+//Lägger till några pokemons som finns med från början
+
+void PopulatePokemonsList ()
+{
+    Pokemon pokemon1 = new();
+    pokemons.Add(pokemon1);
+
+    Pokemon pokemon2 = new();
+    pokemons.Add(pokemon2);
+
+    Pokemon pokemon3 = new();
+    pokemons.Add(pokemon3);
+
+    Pokemon pokemon4 = new();
+    pokemons.Add(pokemon4);
+}
+
